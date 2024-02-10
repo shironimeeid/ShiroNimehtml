1eb60007c5b16bdd722a79fd9274e154a6c1f204
Shironee.XRC <shirokochan1030@gmail.com>
2024-02-10 18:51:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 document.getElementById('redirectToDjango').addEventListener('click', function() {
-    window.location.href = 'https://anime-toddler-simultaneously-corn.trycloudflare.com';
+    window.location.href = 'https://df18076b-0b43-43af-905e-48cf1842cc85-00-2rfhu0zixp4ke.worf.replit.dev/';
 });
